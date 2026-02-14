fc5e0aba508d13621a309a5c0bc8ab6a5d8111a5
Ff-17071 <FCSgoodlife@outlook.com>
2026-02-14 20:47:17 +0800

    day1, study +-*/ and study + ,很有趣的一次学习，测试的时候也没报错，自己的命令也算是熟悉了起来，day2 加油

--- study1.py
+++ study1.py
@@ -1,1 +1,17 @@
-c = a + b //result c
+print("Hi fengchaoseng, welcome to full stack learning route!")
+
+a = 2
+b = 2
+r = 3.1415926
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+s = r * r * a
+print("a + b =", c)
+print("a - b =", d)
+print("a * b =", e)
+print("a / b =", f)
+print("圆s的面积是", s)
+print("s > e = c > f > d" + "= 0")
+
