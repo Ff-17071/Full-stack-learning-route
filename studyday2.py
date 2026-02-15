94bc2e0556e279ff0ad0febfd39bb61fe448f0cf
Ff-17071 <FCSgoodlife@outlook.com>
2026-02-15 15:42:02 +0800

    今天根据计划学习了字符串的大upoer()小lower()写转换，以及去除input的前后空白，也有两个分左lstrip右rstrip,并且完成了一个命令行交互的成绩判断，根据if、elif、else，今晚继续学习更加深入的input格式转换，fuck you

---
+++ studyday2.py
@@ -0,0 +1,14 @@
+name = "Fcs love you "  #字符串
+print("大写的字母是",name.upper())
+print("小写的字母是",name.lower())
+print("需要学习的语言有：\t\nPython\nrust\t\nnodejs\t\npostgreSQL")
+print(name.strip())  #去除字符串前后的空格，lstrip()去除左边，rstrip()去除右边
+
+prompt = input("Hi boys, please iput you socre")
+user_input = float(prompt) #.strip().lower()  #去除用户输入的前后空格,并且输入的字母转换成小写
+if user_input < 60:
+    print("you are failrd")
+elif 60 <= user_input < 90:
+    print("you are success")
+else:
+    print("you are good")
